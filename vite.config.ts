cfe3b7230a017fc674fcf6edbaa43cc515128b8a
OsmarM <imamej705@gmail.com>
2026-04-25 07:35:11 -0600

    Point API to localhost 6060

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:7070',
+        target: 'http://localhost:6060',
         changeOrigin: true,
       },
     },
